caeba641dcdb809c65dc8e79d3da82616e9e49e9
Arnaud S <soulier-arnaud@hotmail.fr>
2022-08-03 16:31:44 +0300

    POC

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ is_rpi = uname().machine == 'aarch64'
 if is_rpi:
     run('cd / && ./media/refresh.sh', shell=True)
     repo = dir_prod
+dead = False
 
 # Logs
 basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
@@ -31,48 +32,60 @@ logger = getLogger(__name__)
 
 
 class QBittorrent():
+    qb = None
+
     def start(self):
         run('sudo systemctl start qbittorrent-nox', shell=True)
         logger.info("qBittorrent - starting...")
         sleep(2)
 
     def init(self):
-        qb = Client(qb_addr)
-        qb.login(qb_user, qb_pass)
+        self.qb = Client(qb_addr)
+        self.qb.login(qb_user, qb_pass)
         logger.info("qBittorrent - connected")
+        self.clean_torrents()
 
     def stop(self):
         run('sudo systemctl stop qbittorrent-nox', shell=True)
         logger.info("qBittorrent - stopping...")
 
-    def size_format(self, b, factor=1024, suffix="B"):
+    def size_format(self, b, factor=1024, suffix="o"):
         for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
             if b < factor:
-                return f"{b:.2f}{unit}{suffix}"
+                return f"{b:.2f} {unit}{suffix}"
             b /= factor
-        return f"{b:.2f}Y{suffix}"
+        return f"{b:.2f} Y{suffix}"
 
-    def add_from_torrent_file(self, torrent):
+    def download_from_torrent_file(self, torrent):
         self.qb.download_from_file(torrent)
 
-    def add_from_magnet_link(self, magnet):
+    def download_from_magnet_link(self, magnet):
         self.qb.download_from_link(magnet)
 
     def get_torrents(self):
         return self.qb.torrents()
 
-    def get_torrent(self, infohash):
-        return self.qb.get_torrent(infohash)
+    def get_torrent(self, info_hash):
+        for t in self.get_torrents():
+            if t['hash'] == info_hash:
+                return t
 
-    def delete_torrent(self, infohash):
-        return self.qb.delete_torrent(infohash)
+    def clean_torrents(self):
+        self.qb.delete_all_permanently()
 
-    def log_torrent(self, infohash):
-        torrent = self.get_torrent(infohash)
-        return dict(torrent=f"Torrent: {torrent['name']}",
-                    size=f"Size: {self.size_format(torrent['total_size'])}",
-                    speed=f"Speed: {self.size_format(torrent['dlspeed'])}/s",
-                    progress=f"Progress: {torrent['progress'] * 100} %")
+    def delete_torrent(self, info_hash):
+        self.qb.delete(info_hash)
+
+    def log_torrent(self, info_hash):
+        torrent = self.get_torrent(info_hash)
+        size = f"Size: {self.size_format(torrent['total_size'])}"
+        speed = f"Speed: {self.size_format(torrent['dlspeed'])}/s"
+        progress = f"Progress: {torrent['progress'] * 100:.2f} %"
+        return dict(
+            name=torrent['name'],
+            details=f"{size}\n{speed}\n{progress}",
+            done=torrent['progress'] == 1
+        )
 
 
 def mediagram():
@@ -84,10 +97,14 @@ def mediagram():
     bot = TeleBot(token)
     bot.delete_my_commands(scope=None, language_code=None)
     bot.set_my_commands(
-        commands=[types.BotCommand("alive", "Check if running"),
-                  types.BotCommand("stop", "Kill switch")]
-    )
+        commands=[types.BotCommand("help", "Description"),
+                  types.BotCommand("alive", "HealthCheck"),
+                  types.BotCommand("list", "File list of files (TODO)"),
+                  types.BotCommand("delete", "Delete file(s) (TODO)"),
+                  types.BotCommand("stop", "KillSwitch"),
+                  types.BotCommand("restart", "Restart")])
     started = dt.fromtimestamp(now()).strftime("%Y-%m-%d %H:%M:%S")
+    bot.send_message(chat_id, f"Started.")
     logger.info("Mediagram - initialized")
 
     @bot.message_handler(commands=['start', 'alive'])
@@ -96,33 +113,72 @@ def mediagram():
             bot.send_message(chat_id, f"Started at {started}\nRunning...")
             logger.info(message.text)
 
-    @bot.message_handler(commands=['stop'])
-    def stop(message):
+    @bot.message_handler(commands=['stop', 'restart'])
+    def kill(message):
         if message.chat.id == chat_id:
-            bot.send_message(chat_id, f"Stopped.")
+            global dead
+            if message.text == '/stop':
+                bot.send_message(chat_id, f"Stopped.")
+                dead = True
+            else:
+                bot.send_message(chat_id, f"Restarting...")
             logger.info(message.text)
             if is_rpi:
                 qb.stop()
             bot.stop_polling()
 
+    @bot.message_handler(commands=['help'])
+    def help(message):
+        if message.chat.id == chat_id:
+            bot.send_message(
+                chat_id, f"Send a .torrent file or a magnet link to download it on your Raspberry Pi.")
+            logger.info(message.text)
+
     @bot.message_handler(func=lambda message: message.document.mime_type == 'application/x-bittorrent', content_types=['document'])
-    def upload(message):
+    def upload_torrent_file(message):
         if message.chat.id == chat_id:
             file_info = bot.get_file(message.document.file_id)
-            file = bot.download_file(file_info.file_path)
+            torrent = bot.download_file(file_info.file_path)
             if not path.exists(repo):
-                logger.info(f"Missing directory: '{repo}'")
+                logger.error(f"Missing directory: '{repo}'")
             else:
-                torrent = repo + message.document.file_name
-                with open(torrent, 'wb') as f:
-                    f.write(file)
-                msg = bot.send_message(
-                    chat_id, "Torrent uploaded.\nStart downloading...")
-                logger.info(f"/upload: '{message.document.file_name}'")
-                #bot.edit_message_text('edited', chat_id, msg.id)
+                logger.info(
+                    f"/upload_torrent_file: '{message.document.file_name}'")
+                qb.download_from_torrent_file(torrent)
+                download_manager(message, "Torrent file")
+
+    @bot.message_handler(func=lambda message: message.text.startswith('magnet:?xt='), content_types=['text'])
+    def upload_magnet_link(message):
+        if message.chat.id == chat_id:
+            if not path.exists(repo):
+                logger.error(f"Missing directory: '{repo}'")
+            else:
+                logger.info(f"/upload_magnet_link: '{message.text}'")
+                qb.download_from_magnet_link(message.text)
+                download_manager(message, "Magnet link")
+
+    def download_manager(src_message, torrent_type):
+        info_hash = qb.get_torrents()[0]['hash']
+        info = qb.log_torrent(info_hash)
+        name = info['name']
+        logger.info(f"/download: '{name}'")
+        uploaded = f"Torrent: {name}\n{torrent_type} uploaded.\nStart downloading...\n"
+        msg = bot.reply_to(src_message, f"{uploaded}{info['details']}")
+        while not info['done']:
+            sleep(1)
+            new_info = qb.log_torrent(info_hash)
+            if info != new_info:
+                info = new_info
+                bot.edit_message_text(
+                    f"{uploaded}{info['details']}", chat_id, msg.id)
+        qb.delete_torrent(info_hash)
+        bot.edit_message_text(
+            f"{uploaded}{info['details']} - Done!", chat_id, msg.id)
+        logger.info(f"/done: '{name}'")
 
     bot.infinity_polling(skip_pending=True)
 
 
 if __name__ == '__main__':
-    mediagram()
+    while not dead:
+        mediagram()
