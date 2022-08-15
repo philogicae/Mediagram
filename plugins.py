3da986ac9256f0638c3757fab69de8a157081a28
Arnaud S <soulier-arnaud@hotmail.fr>
2022-08-15 21:27:30 +0300

    Plugins 2

--- plugins.py
+++ plugins.py
@@ -2,9 +2,7 @@ from pythonopensubtitles.opensubtitles import OpenSubtitles
 from rarbgapi import RarbgAPI
 
 
-class SrtSearch:
-    LBD = lambda sub: sub['nb_downloads']
-
+class SubtitlesSearch:
     def __init__(self, user, password):
         self.api = OpenSubtitles()
         self.api.login(user, password)
@@ -17,25 +15,27 @@ class SrtSearch:
             name=sub['MovieReleaseName'],
             nb_downloads=int(sub['SubDownloadsCnt']),
             lang=sub['SubLanguageID'],
-            format=sub['SubFormat']
+            ext=sub['SubFormat']
         ) for sub in subtitles]
-        return sorted(filtered, key=self.LBD, reverse=True)
+        return sorted(filtered, key=lambda sub: sub['nb_downloads'], reverse=True)[:10]
 
-    def download(self, id, name, path):
-        self.api.download_subtitles([id], override_filenames={
-                                    id: name}, output_directory=path)
+    def download(self, sub, name, path):
+        ids, names = [sub['id']], {sub['id']: f"{name}.{sub['ext']}"}
+        return self.api.download_subtitles(ids, names, path, extension=sub['ext'])
 
 
 class TorrentSearch:
+    CATEGORIES = [14, 48, 17, 44, 45, 50, 51, 52, 54, 42, 46, 18, 41, 49]
+
     def __init__(self):
         self.api = RarbgAPI()
 
     def query(self, query):
         torrents = self.api.search(
-            search_string=query, extended_response=True, sort='seeders')
+            search_string=query, extended_response=True, sort='seeders', categories=self.CATEGORIES)
         return [dict(
             name=t.filename,
             peers=f'{t.seeders}/{t.leechers}',
             size=f'{t.size / 2**30:.2f} Go',
             magnet=t.download
-        ) for t in torrents]
+        ) for t in torrents if t.seeders > 1][:10]
